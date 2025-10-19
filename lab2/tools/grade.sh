e342a1ac2a462f9a35fbadf7befde203e43c7ade
Cong-Fanghao <2310682@mail.nankai.edu.cn>
2025-10-19 17:57:56 +0800

    把模板中实现多级链表伙伴系统

--- lab2/tools/grade.sh
+++ lab2/tools/grade.sh
@@ -358,16 +358,21 @@ echo "<<<<<<<<<<<<<<< here_run_qemu <<<<<<<<<<<<<<<<<<"
 run_qemu
 echo "<<<<<<<<<<<<<<< here_run_check <<<<<<<<<<<<<<<<<<"
 
-pts=5
-quick_check 'check physical_memory_map_information'                                         \
-    'memory management: best_fit_pmm_manager'                     \
-    '  memory: 0x0000000008000000, [0x0000000080000000, 0x0000000087ffffff].'                                  \
+# pts=5
+# quick_check 'check physical_memory_map_information'                                         \
+#     'memory management: best_fit_pmm_manager'                     \
+#     '  memory: 0x0000000008000000, [0x0000000080000000, 0x0000000087ffffff].'                                  \
 
-pts=20
-quick_check 'check_best_fit'                                       \
-    'check_alloc_page() succeeded!'                                  \
-    'satp virtual address: 0xffffffffc0204000'                       \
-    'satp physical address: 0x0000000080204000'                      \
+# pts=20
+# quick_check 'check_best_fit'                                       \
+#     'check_alloc_page() succeeded!'                                  \
+#     'satp virtual address: 0xffffffffc0204000'                       \
+#     'satp physical address: 0x0000000080204000'                      \
+
+pts=5
+quick_check 'check physical_memory_map_information' \
+    'memory management: buddy_system_pmm_manager' \
+    '  memory: 0x0000000008000000, [0x0000000080000000, 0x0000000087ffffff].'
 
 # pts=5
 # quick_check 'check ticks'                                       \
